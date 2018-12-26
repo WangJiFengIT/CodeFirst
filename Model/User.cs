4ff8bd0420dd67a723c48d80377ae6fed8a3fc31
WangJiFengIT <2535152323@qq.com>
2018-12-26 10:24:31 +0800

    基于Asp.Net三层架构实现CodeFirst

---
+++ Model/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Model
+{
+    [Table("Sys_User")]
+    public class User
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 登录名
+        /// </summary>
+        [Required] //必填项（非空）
+        [MaxLength(50)] //最大长度（50）
+        public string LoginName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
---
+++ Orm/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Orm
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base(GetConnectionString()) { }
+
+        private static string GetConnectionString()
+        {
+            return "SqlServerConnectionString";
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }//建立实体类与表的映射关系
+    }
+}
